baa64159d4b001581366ffda161fa7c536923c9f
Wesley <weslley182@gmail.com>
2016-10-15 22:59:55 -0300

    Finalizando repositorio

---
+++ App/EstruturaDemo.Infra.Repository/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace EstruturaDemo.Infra.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EstruturaDemo.Infra.Repository.EF.EstruturaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(EstruturaDemo.Infra.Repository.EF.EstruturaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
